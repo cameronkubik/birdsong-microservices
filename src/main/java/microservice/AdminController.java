1a35d64a4cd87e3dc58406a799c7b44820db8e12
Cole McKiever <colemckiever@gmail.com>
2018-11-28 18:43:57 -0600

    Starting DB access

--- src/main/java/microservice/AdminController.java
+++ src/main/java/microservice/AdminController.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.*;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,12 @@ public class AdminController {
     String page;
     @Value("${tmdb.region}")
     String region;
+    @Value("${db2.url}")
+    String db2Url;
+    @Value("${db2.user}")
+    String db2User;
+    @Value("${db2.password}")
+    String db2Pass;
     //https://api.themoviedb.org/3/movie/upcoming?api_key=12ab19db903903ba44c5e6bf73694e3c&language=en-US&page=1&region=US
     //https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&query=<<search_query>>&page=1&include_adult=false&region=<<region>>
     
@@ -142,8 +149,33 @@ public class AdminController {
 
     @RequestMapping("/admin/save-movie")
     public boolean saveMovieAsNowShowing(@RequestParam(value="movieId", defaultValue="-1") String movieIDString) throws Exception {
+        Movie saveMovie;
+        String url = db2Url;
+        String user = db2User;
+        String password = db2Pass;
+        Connection con;
+        Statement stmt;
+        ResultSet rs;
+
         // find movie in allMovies
+        for (Movie movieObject: allMovies) {
+            if (movieObject.getId() == Long.valueOf(movieIDString)) {
+                saveMovie = movieObject;
+            }
+        }
+
         // get connection with DB2
+        // Load the driver
+        Class.forName("com.ibm.db2.jcc.DB2Driver");
+
+        // Create the connection using the IBM Data Server Driver for JDBC and SQLJ
+        con = DriverManager.getConnection (url, user, password); 
+        // Commit changes manually
+        con.setAutoCommit(false);
+        // Create the Statement
+        stmt = con.createStatement();
+        // Execute a query and generate a ResultSet instance
+        stmt.executeQuery("SELECT EMPNO FROM EMPLOYEE");
         // create query strings
         // execute queries
 
