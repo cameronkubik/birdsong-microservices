86a1f7a6a5b9568a207ea4f057875dc2898beaca
C.Kubik <cam.kubik@gmail.com>
2018-12-01 15:00:16 -0600

    working hello world

---
+++ src/main/java/microservice/HelloWorldController.java
@@ -0,0 +1,15 @@
+package microservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
