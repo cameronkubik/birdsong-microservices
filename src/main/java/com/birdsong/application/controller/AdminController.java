edd128a8fd8f7655a9e9b7487e98f11f8e4ca42e
C.Kubik <cam.kubik@gmail.com>
2018-12-03 02:44:45 -0600

    start welcome transactions

--- src/main/java/com/birdsong/application/controller/AdminController.java
+++ src/main/java/com/birdsong/application/controller/AdminController.java
@@ -96,11 +96,7 @@ public class AdminController {
         model.addAttribute("header", aboutUsContent.getHeader());
         model.addAttribute("subHeader", aboutUsContent.getSubHeader());
         model.addAttribute("body", aboutUsContent.getBody());
-<<<<<<< HEAD
         aboutUsService.postAboutUs(aboutUsContent);
-=======
-        
->>>>>>> e4f06f3efef27b0544e7356d03e71ed480d91304
         return "adminAboutUs";
     }
 
@@ -127,7 +123,7 @@ public class AdminController {
         model.addAttribute("youtube", footer.getYoutube());
 
         service.postFooterData(footer);
-        
+
         return "adminFooter";
     }
 }
---
+++ src/main/java/com/birdsong/application/model/HomeContent.java
@@ -0,0 +1,32 @@
+package com.birdsong.application.model;
+
+import com.birdsong.application.service.AboutUsScreenService;;
+
+public class HomeContent {
+
+    private AboutUsScreenService service = new AboutUsScreenService();
+
+    private String welcome;
+    private String specialEvents;
+
+    public HomeContent() {
+        this.welcome = service.getHeaderFromDb();
+        this.specialEvents = service.getSubHeaderFromDb();
+    }
+
+    public String getwelcome() {
+        return this.welcome;
+    }
+
+    public void setwelcome(String _welcome) {
+        this.welcome = _welcome;
+    }
+
+    public String getspecialEvents() {
+        return this.specialEvents;
+    }
+
+    public void setspecialEvents(String _specialEvents) {
+        this.specialEvents = _specialEvents;
+    }
+}
